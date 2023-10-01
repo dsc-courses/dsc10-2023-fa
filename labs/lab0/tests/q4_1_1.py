dca273bd7bf02cd211d3d0b6f3be62430109bd13
Nate del Rosario <ndelrosx6@gmail.com>
2023-09-30 19:33:59 -0700

    Addition: lab 0 released

---
+++ labs/lab0/tests/q4_1_1.py
@@ -0,0 +1,7 @@
+test = {   'name': 'q4_1_1',
+    'points': None,
+    'suites': [   {   'cases': [{'code': '>>> import math\n>>> math.isclose(abel_distance_from_average_hrs, 0.73, rel_tol=1e-4)\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
