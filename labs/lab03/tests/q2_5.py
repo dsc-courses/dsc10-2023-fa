117df87a2a5ca6c42896b95e4e4a357461bc3ab0
Weiyue Li <wel019@ucsd.edu>
2023-10-22 02:14:30 -0700

    update q2_1 tests

--- labs/lab03/tests/q2_5.py
+++ labs/lab03/tests/q2_5.py
@@ -1,21 +1,18 @@
 test = {   'name': 'q2_5',
     'points': None,
-    'suites': [   {   'cases': [   {   'code': ">>> (result == 'More Large coffees') or (result ==  'More Extra Large coffees') or (result == 'Same amount') # Check spelling and capitalization!\n"
-                                               'True',
+    'suites': [   {   'cases': [   {   'code': ">>> (result == 'More Large coffees') or (result ==  'More Extra Large coffees') or (result == 'Same amount') # Check spelling and capitalization!\n",
                                        'hidden': False,
                                        'locked': False},
                                    {   'code': ">>> ten_coffees = np.array(['Large', 'Extra Large', 'Small', 'Large', 'Medium', 'Small', 'Extra Large', 'Small', 'Small', 'Large'])\n"
                                                '>>> ten_coffees_prices = bpd.DataFrame().assign(Size=ten_coffees)\n'
                                                '>>> ten_coffees_prices = ten_coffees_prices.assign(Price=ten_coffees_prices.get("Size").apply(coffee_price))\n'
-                                               ">>> large_or_xl(ten_coffees_prices) == 'More Large coffees'\n"
-                                               'True',
+                                               ">>> large_or_xl(ten_coffees_prices) == 'More Large coffees'\n",
                                        'hidden': False,
                                        'locked': False},
                                    {   'code': ">>> seven_coffees = np.array(['Medium', 'Extra Large', 'Large', 'Medium', 'Small', 'Extra Large', 'Large'])\n"
                                                '>>> seven_coffees_prices = bpd.DataFrame().assign(Size=seven_coffees)\n'
                                                '>>> seven_coffees_prices = seven_coffees_prices.assign(Price=seven_coffees_prices.get("Size").apply(coffee_price))\n'
-                                               ">>> large_or_xl(seven_coffees_prices) == 'Same amount'\n"
-                                               'True',
+                                               ">>> large_or_xl(seven_coffees_prices) == 'Same amount'\n",
                                        'hidden': False,
                                        'locked': False}],
                       'scored': True,
--- labs/lab03/tests/q4_4.py
+++ labs/lab03/tests/q4_4.py
@@ -1,3 +1,3 @@
 test = {   'name': 'q4_4',
     'points': None,
-    'suites': [{'cases': [{'code': '>>> np.isclose(winning_prob, 0.3214285714285714)\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
+    'suites': [{'cases': [{'code': '>>> np.isclose(winning_prob, 0.3214285714285714)\n', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
